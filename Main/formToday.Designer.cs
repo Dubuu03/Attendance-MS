4efd2fa688c5ffbb7496ea350968e6857c54ae91
Drix <narcisodrix@gmail.com>
2024-11-08 23:49:39 +0800

    Initial commit

---
+++ Main/formToday.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Main
+{
+    partial class formToday
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbDate = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.timerTime = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnExportAttendace = new System.Windows.Forms.Button();
+            this.txtSearchAttendance = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dgvAttendance = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttendance)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbDate
+            // 
+            this.lbDate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbDate.AutoSize = true;
+            this.lbDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDate.Location = new System.Drawing.Point(127, 30);
+            this.lbDate.Name = "lbDate";
+            this.lbDate.Size = new System.Drawing.Size(68, 25);
+            this.lbDate.TabIndex = 4;
+            this.lbDate.Text = "Date:";
+            // 
+            // lbTime
+            // 
+            this.lbTime.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.Location = new System.Drawing.Point(1056, 30);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(70, 25);
+            this.lbTime.TabIndex = 5;
+            this.lbTime.Text = "Time:";
+            // 
+            // timerTime
+            // 
+            this.timerTime.Interval = 1;
+            this.timerTime.Tick += new System.EventHandler(this.timerTime_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.dgvAttendance);
+            this.panel1.Location = new System.Drawing.Point(132, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1129, 620);
+            this.panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(90)))), ((int)(((byte)(85)))));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnExportAttendace);
+            this.panel2.Controls.Add(this.txtSearchAttendance);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1129, 72);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnExportAttendace
+            // 
+            this.btnExportAttendace.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnExportAttendace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(191)))), ((int)(((byte)(207)))));
+            this.btnExportAttendace.FlatAppearance.BorderSize = 0;
+            this.btnExportAttendace.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExportAttendace.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportAttendace.ForeColor = System.Drawing.Color.White;
+            this.btnExportAttendace.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExportAttendace.Location = new System.Drawing.Point(967, 9);
+            this.btnExportAttendace.Name = "btnExportAttendace";
+            this.btnExportAttendace.Padding = new System.Windows.Forms.Padding(12, 0, 12, 0);
+            this.btnExportAttendace.Size = new System.Drawing.Size(116, 39);
+            this.btnExportAttendace.TabIndex = 9;
+            this.btnExportAttendace.Text = "Export";
+            this.btnExportAttendace.UseVisualStyleBackColor = false;
+            // 
+            // txtSearchAttendance
+            // 
+            this.txtSearchAttendance.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtSearchAttendance.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchAttendance.Location = new System.Drawing.Point(656, 12);
+            this.txtSearchAttendance.MaxLength = 255;
+            this.txtSearchAttendance.Multiline = true;
+            this.txtSearchAttendance.Name = "txtSearchAttendance";
+            this.txtSearchAttendance.Size = new System.Drawing.Size(295, 36);
+            this.txtSearchAttendance.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(557, 17);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 25);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Search:";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label10.Location = new System.Drawing.Point(23, 23);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(140, 25);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "Today\'s Log";
+            // 
+            // dgvAttendance
+            // 
+            this.dgvAttendance.AllowUserToAddRows = false;
+            this.dgvAttendance.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvAttendance.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAttendance.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvAttendance.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvAttendance.Location = new System.Drawing.Point(0, 71);
+            this.dgvAttendance.Name = "dgvAttendance";
+            this.dgvAttendance.ReadOnly = true;
+            this.dgvAttendance.Size = new System.Drawing.Size(1129, 549);
+            this.dgvAttendance.TabIndex = 2;
+            // 
+            // formToday
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1406, 819);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.lbDate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "formToday";
+            this.Text = "formToday";
+            this.Load += new System.EventHandler(this.formToday_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttendance)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbDate;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Timer timerTime;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnExportAttendace;
+        private System.Windows.Forms.TextBox txtSearchAttendance;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dgvAttendance;
+    }
+}
---
+++ Main/formToday.cs
@@ -0,0 +1,93 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class formToday : Form
+    {
+        public formToday()
+        {
+            InitializeComponent();
+            LoadAttendanceToday();
+            this.txtSearchAttendance.TextChanged += new EventHandler(this.txtSearchAttendance_TextChanged);
+            timerTime.Start();
+
+        }
+
+        private void formToday_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+            LoadAttendanceToday();
+        }
+        public void RefreshAttendance()
+        {
+            LoadAttendanceToday();
+        }
+
+        private void LoadAttendanceToday(string searchTerm = "")
+        {
+            string query;
+
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                query = "SELECT StudentID, Name, Course, `Year`, Section, Date, TimeInAM, TimeOutAM, TimeInPM, TimeOutPM, Remarks FROM tblattendance WHERE Date = CURDATE()";
+            }
+            else
+            {
+                query = "SELECT StudentID, Name, Course, `Year`, Section, Date, TimeInAM, TimeOutAM, TimeInPM, TimeOutPM, Remarks " +
+                  "FROM tblattendance " +
+                  "WHERE (Name LIKE '%" + searchTerm + "%' OR StudentID LIKE '%" + searchTerm + "%') " +
+                  "AND Date = CURDATE()";
+
+            }
+
+            Database db = new Database();
+            DataTable dt = db.ExecuteQuery(query);
+            dgvAttendance.DataSource = dt;
+
+            if (dgvAttendance.Columns.Contains("AttendanceID"))
+            {
+                dgvAttendance.Columns["AttendanceID"].Visible = false;
+            }
+            // Set header 
+            dgvAttendance.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
+            dgvAttendance.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgvAttendance.ColumnHeadersDefaultCellStyle.Font = new Font(dgvAttendance.Font, FontStyle.Bold);
+
+            // alternating row colors
+            dgvAttendance.AlternatingRowsDefaultCellStyle.BackColor = Color.LightGray;
+            dgvAttendance.RowsDefaultCellStyle.BackColor = Color.White;
+
+            dgvAttendance.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+        private void txtSearchAttendance_TextChanged(object sender, EventArgs e)
+        {
+            string searchTerm = txtSearchAttendance.Text.Trim();
+            LoadAttendanceToday(searchTerm);
+        }
+
+        private void UpdateTime()
+        {
+            lbDate.Text = "Date: " + DateTime.Now.ToString("MMMM dd, yyyy"); 
+            lbTime.Text = "Time: " + DateTime.Now.ToString("hh:mm:ss tt"); 
+
+        }
+
+        private void timerTime_Tick(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+
+        
+
+
+    }
+}
